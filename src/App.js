902e54e5c0961c6d0db3bf28d43959d336cb862c
Alexander Kallaway <kallaway@me.com>
2017-01-04 07:08:05 +0800

    create a couple of basic components

--- src/App.js
+++ src/App.js
@@ -13,9 +13,40 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		<MainFrame />
       </div>
     );
   }
 }
+
+class MainFrame extends Component {
+	render() {
+		return (
+			<div>
+				<p>Trying out putting one component into another</p>
+				<Editor />
+				<Preview />
+			</div>
+		);
+	}
+}
+
+class Editor extends Component {
+	render() {
+		return (
+			<p>This is Editor</p>
+		);
+	}
+}
+
+class Preview extends Component {
+	render() {
+		return (
+			<p>This is Preview</p>
+		);
+	}
+}
+
+
 
 export default App;
